ed9143c07ee3a5ee2cc447fd65c6c91f25d50fe8
해준 <127281057+ftery0@users.noreply.github.com>
2023-10-04 22:01:33 +0900

    middle save

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css';
 import Login from './logincommpent/login'; 
+import Mypage from './mypage/mypage'
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path="/mypage" element={<Mypage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/mypage/mypage.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+export default function MyPage() {
+    
+
+  return (
+    <div className="mypage">
+        <div className='main'>
+        <div className='profile'>
+        <h1 id='profile_name'>프로필</h1>
+        <div className='profile_img'>
+          <label htmlFor="img_upload">
+            <img id='img' src={require("../img/icon.png")} alt="" />
+            <input type="button" id="profile_correction" ></input>
+          </label>
+          <input type="file" id="img_upload" style={{ display: 'none' }} />
+        </div>
+      </div>
+        </div>
+        <div className='footer'>
+            <div id='writting_button'>
+                <input className='button' type="button"  />
+                <input className='button1' type="button"  />
+            </div>
+        </div>
+    </div>
+  );
+}
