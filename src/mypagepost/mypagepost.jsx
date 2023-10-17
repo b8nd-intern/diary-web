6b53e71eac519acf35a81e42fc42ce92264580cc
해준 <127281057+ftery0@users.noreply.github.com>
2023-10-17 21:36:58 +0900

    mypage

--- src/mypagepost/mypagepost.jsx
+++ src/mypagepost/mypagepost.jsx
@@ -2,24 +2,21 @@ import React, { useEffect } from "react";
 import "./mypagepost.css";
 
 export default function Mypagepost(){
-        useEffect(()=>{
-            const elements = document.querySelectorAll(".post2");
-            elements.forEach((element) => {
-                element.addEventListener("click", handclick);
-              });
-              return () => {
-                elements.forEach((element) => {
-                  element.removeEventListener("click",handclick );
-                });
-              };
-            }, []);
-        
-
-            
+        // useEffect(()=>{
+        //     const elements = document.querySelectorAll(".post2");
+        //     elements.forEach((element) => {
+        //         element.addEventListener("click", handclick);
+        //       });
+        //       return () => {
+        //         elements.forEach((element) => {
+        //           element.removeEventListener("click",handclick );
+        //         });
+        //       };
+        //     }, []);
   function handclick(event) {
     const target = event.target;
     const post = target.classList.contains("post2") ? target: target.closest(".post2"); //이 요소가 "post2"라는 클래스를 가지고 있는지 확인한다 ,
-                                                                                        //만약 클릭한 요소가 "post2" 클래스를 가지고 있으면, 그 요소를 post로 지정한다. 그렇지 않으면, 
+    const newdiv =document.createElement("div");                                        //만약 클릭한 요소가 "post2" 클래스를 가지고 있으면, 그 요소를 post로 지정한다. 그렇지 않으면, 
                                                                                         //부모 요소 중에서 "post2" 클래스를 가진 가장 가까운 요소를 찾아서 post로 설정한다
     if (post) {
       const expandedHeight = "300px";
@@ -48,35 +45,38 @@ export default function Mypagepost(){
                     </select>
                 </span>
                 <div className="post">
-                    <div className="post2"  >
+                    <div className="post2" onClick={handclick} >
                         <span className="month">1월</span>
                         <span className="number">15장</span>
+                        <div className="post_one"></div>
+                        <div className="post_two"></div>
+                        <div className="post_thr"></div>
                     </div>
-                    <div className="post2" >
+                    <div className="post2" onClick={handclick} >
                         <span className="month">2월</span>
                         <span className="number">20장</span>
                     </div>
-                    <div className="post2" >
+                    <div className="post2" onClick={handclick}>
                         <span className="month">3월</span>
                         <span className="number">36장</span>
                     </div>
-                    <div className="post2">
+                    <div className="post2" onClick={handclick}>
                         <span className="month">4월</span>
                         <span className="number">40장</span>
                     </div>
-                    <div className="post2">
+                    <div className="post2" onClick={handclick}>
                         <span className="month">5월</span>
                         <span className="number">20장</span>
                     </div>
-                    <div className="post2" >
+                    <div className="post2" onClick={handclick}>
                         <span className="month">6월</span>
                         <span className="number">34장</span>
                     </div>
-                    <div className="post2">
+                    <div className="post2" onClick={handclick}>
                         <span className="month">7월</span>
                         <span className="number">10장</span>
                     </div>
-                    <div className="post2" >
+                    <div className="post2" onClick={handclick}>
                         <span className="month">8월</span>
                         <span className="number">12장</span>
                     </div>
--- src/name_login/name_login.jsx
+++ src/name_login/name_login.jsx
@@ -1,20 +1,87 @@
-import "./name_login.css"
+import React, { useState } from "react";
+import "./name_login.css";
 import { Link } from "react-router-dom";
 
-export default function NameLogin(){
-    return(
-        <div className="Name_Login">
-            <div className="group2">
-                <div id="por"></div>
-                    <div id="login_write">
-                        안녕하세요 함께 오늘 하루를 작성해볼까요?
-                    </div>
-                <form className="googleLog">
-                <input className="write_name" type="text" placeholder="이름을 적어주세요"/>
-                <Link to="/mainhome" className="Button_home"><input type="submit" value="시작하기"></input></Link> 
-                </form>
-        
-            </div>
+export default function NameLogin() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [name, setName] = useState("");
+  const [startDisabled, StartDisabled] = useState(true);
+
+  const onUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        setImageSrc(reader.result || null);
+        checkStartEnabled();
+      };
+    }
+  };
+
+  const onNameChange = (e) => {
+    const newName = e.target.value;
+    setName(newName);
+    checkStartEnabled();
+  };
+
+  const checkStartEnabled = () => {
+    if (name) { //이름이 써져있다면
+      StartDisabled(false);
+    }
+    else if(imageSrc){ //프로필이 설정이 되었다면
+        StartDisabled(false);
+    }
+     else {
+      StartDisabled(true);
+    }
+  };
+  const Starthandclick=()=>{
+    if (name && imageSrc ===" "){
+        alert("프로필과 이름을 입력해주세요")
+    }
+    else if(name === " "){
+        alert("이름을 입력해주세요")
+    }
+    else{
+        // alert("프로필을 입력해주세요")
+    }
+  }
+  
+  return (
+    <div className="Name_Login">
+    
+      <div className="group2">
+        <div id="por">
+          {imageSrc && <img src={imageSrc} alt="Preview" />}
         </div>
-    )
+        <form action="" id="por_form">
+          <label htmlFor="file">
+            <div className="btn-upload">프로필 설정</div>
+          </label>
+          <input type="file" name="file" id="file" onChange={onUpload} />
+        </form>
+        <div id="login_write">
+          안녕하세요 함께 오늘 하루를 작성해볼까요?
+        </div>
+        <form className="googleLog">
+          <input
+            className="write_name"
+            type="text"
+            placeholder="이름을 적어주세요"
+            value={name}
+            onChange={onNameChange}
+          />
+          <Link to="/mainhome" className="Button_home">
+            <input type="submit"
+              value="시작하기"
+             onClick={Starthandclick} 
+             maxLength={5}
+             disabled={startDisabled}/>
+          </Link>
+        </form>
+      </div>
+    </div>  
+  );
 }
