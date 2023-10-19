690eb7d2e8d409cfe477986f844a23790ac607e4
your-kitten <130333571+your-kitten@users.noreply.github.com>
2023-10-19 21:56:18 +0900

    fix textareaMaintain

--- src/components/Writing/writing.jsx
+++ src/components/Writing/writing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../footer/footer";
 import "./writing.css";
 import Writingicon from "../../noteIcon/writingicon";
@@ -7,6 +7,8 @@ import Calendar from "../Calendar/calender";
 const Openwriting = () => {
   const [selectedImage, setSelectedImage] = useState("");
   const [selectedColor, setSelectedColor] = useState("");
+  const [isPublic, setIsPublic] = useState(false);
+  const [noteText, setNoteText] = useState("");
 
   const handleImageClick = (imageUrl) => {
     setSelectedImage(imageUrl);
@@ -16,14 +18,33 @@ const Openwriting = () => {
     setSelectedColor(color);
   };
 
+  const handlePublicClick = () => {
+    setIsPublic(true);
+  };
+
+  const handlePrivateClick = () => {
+    setIsPublic(false);
+  };
+
+  useEffect(() => {
+    const savedNoteText = localStorage.getItem("noteText");
+    if (savedNoteText) {
+      setNoteText(savedNoteText);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("noteText", noteText);
+  }, [noteText]); 
+
   return (
     <div>
       <Footer />
       <div className="openWritingNotepad">
         <div>
           <div className="openWritingtext">
-            <span>공개</span>
-            <span>비공개</span>
+            <span style={{ color: isPublic ? "#000000" : "#A1A1A1" }} onClick={handlePublicClick}>공개</span>
+            <span style={{ color: !isPublic ? "#000000" : "#A1A1A1" }} onClick={handlePrivateClick}>비공개</span>
           </div>
           <div>
             <Writingicon onImageClick={handleImageClick} />
@@ -36,7 +57,8 @@ const Openwriting = () => {
           </div>
           <div className="openNotepad" style={{ backgroundColor: selectedColor }}>
             <img src={selectedImage} alt=""/>
-            <textarea name="Notepad" id="" cols="30" rows="10" style={{ backgroundColor: selectedColor }}></textarea>
+            <textarea name="Notepad" id="" cols="30" rows="10" style={{ backgroundColor: selectedColor }} 
+            value={noteText} onChange={(e) => setNoteText(e.target.value)}></textarea>
           </div>
           <div className="openNotepadSubmit">
             <button type="button">올리기</button>
