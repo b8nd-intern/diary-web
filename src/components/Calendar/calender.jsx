bfb973428e008337d32e55612276def83cd8115d
your-kitten <130333571+your-kitten@users.noreply.github.com>
2023-10-23 19:33:20 +0900

    fix calendar date, year, design

--- src/components/Calendar/calender.jsx
+++ src/components/Calendar/calender.jsx
@@ -1,32 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "./calender.css";
+import moment from "moment";
 
 const Calender = () => {
   const [today] = useState(new Date());
+  const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월`;
 
   useEffect(() => {
-    const prevButton = document.querySelector(
-      ".react-calendar__navigation__prev2-button"
+    const navigation = document.querySelector(
+      ".react-calendar__navigation__label"
     );
     const nextButton = document.querySelector(
       ".react-calendar__navigation__next2-button"
     );
-    if (prevButton || nextButton) {
+    const prevButton = document.querySelector(
+      ".react-calendar__navigation__prev2-button"
+    );
+    if (prevButton || nextButton || navigation) {
       prevButton.remove();
       nextButton.remove();
+      navigation.remove();
     }
   }, []);
+
   const tileContent = ({ date, view }) => {
     if (view === "month" && date.getDate() === today.getDate()) {
       return <div className="highlighted-date"></div>;
     }
     return null;
   };
-
   return (
     <div className="calenderBackground">
-      <Calendar tileContent={tileContent} />
+      <span>{formattedDate}</span>
+      <Calendar
+        tileContent={tileContent}
+        formatDay={(date) => moment(date).format("D")}
+      />
     </div>
   );
 };
