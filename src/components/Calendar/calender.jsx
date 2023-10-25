3d4abd54d35054f334c1060ebfd018395be254d0
your-kitten <130333571+your-kitten@users.noreply.github.com>
2023-10-25 20:54:30 +0900

    fix calendar change date

--- src/components/Calendar/calender.jsx
+++ src/components/Calendar/calender.jsx
@@ -4,7 +4,7 @@ import "./calender.css";
 import moment from "moment";
 
 const Calender = () => {
-  const [today] = useState(new Date());
+  const [today, setDate] = useState(new Date());
   const formattedDate = `${today.getFullYear()}년 ${today.getMonth() + 1}월`;
 
   useEffect(() => {
@@ -24,17 +24,57 @@ const Calender = () => {
     }
   }, []);
 
+  const [showHighlightedDate, setShowHighlightedDate] = useState(true);
+
   const tileContent = ({ date, view }) => {
     if (view === "month" && date.getDate() === today.getDate()) {
-      return <div className="highlighted-date"></div>;
+      if (date.getMonth() !== today.getMonth()) {
+        return null;
+      } 
+      return showHighlightedDate ? (
+        <div className="highlighted-date"></div>
+      ) : null;
     }
     return null;
   };
+
+  const handlePrevButtonClick = () => {
+    const prevMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+    setDate(prevMonth);
+    setShowHighlightedDate(false);
+  };
+
+  const handleNextButtonClick = () => {
+    const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+    setDate(nextMonth);
+    setShowHighlightedDate(false);
+  };
+
   return (
     <div className="mainBack">
       <div className="calenderBackground">
         <span>{formattedDate}</span>
         <Calendar
+          prevLabel={
+            <button
+              aria-label="이전 달"
+              className="react-calendar__navigation__prev-button"
+              type="button"
+              onClick={handlePrevButtonClick}
+            >
+              ‹
+            </button>
+          }
+          nextLabel={
+            <button
+              aria-label="다음 달"
+              className="react-calendar__navigation__next-button"
+              type="button"
+              onClick={handleNextButtonClick}
+            >
+              ›
+            </button>
+          }
           tileContent={tileContent}
           formatDay={(locale, date) => moment(date).format("D")}
         />
