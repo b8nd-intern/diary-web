744debbbc192ad24456415470332b96b078d5887
해준 <127281057+ftery0@users.noreply.github.com>
2023-11-28 21:25:35 +0900

    📑 :: 테스트 코드

--- src/constants/Google/google.js
+++ src/constants/Google/google.js
@@ -1,50 +1,47 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
-import CONFIG from "../../config.json"
+import Cookies from "js-cookie";
+import CONFIG from "../../config.json";
 import { showToast } from "../Swal/Toast";
 
-
-
-
 const GOOGLELOGIN = () => {
-  const clientId = process.env.REACT_APP_CLIENT_ID;
   const navigate = useNavigate();
-  const [cookies, setCookie] = useCookies();
+
   const onSuccess = async (response) => {
     try {
       const headers = {
         id_token: `${response.credential}`,
       };
 
-      response = await axios.post(`${CONFIG.serverUrl}/auth/login/google`, null, {
-        headers,
-      });
-      
-      
+      response = await axios.post(
+        `${CONFIG.serverUrl}/auth/login/google`,
+        null,
+        {
+          headers,
+        }
+      );
+
       if (response.status === 200) {
-        
         const { accessToken, refreshToken } = response.data.data;
-        console.log(accessToken);
-        console.log(refreshToken)
-        setCookie("accessToken", accessToken);
-        setCookie("refreshToken", refreshToken);
+        Cookies.set("accessToken", accessToken);
+        Cookies.set("refreshToken", refreshToken);
       }
-      if (response.data.data.isFirst == false) {
+
+      if (response.data.data.isFirst === false) {
         navigate("/mainhome");
       } else {
         navigate("/name_login");
       }
     } catch (error) {
-      showToast("error", "서버오류")
+      showToast("error", "서버오류");
     }
   };
 
   return (
     <div className="google_button">
-      <GoogleOAuthProvider clientId={clientId}>
+      <GoogleOAuthProvider clientId={CONFIG.CLIENTID}>
         <GoogleLogin
           onSuccess={onSuccess}
           onError={() => showToast("error", "실패")}
